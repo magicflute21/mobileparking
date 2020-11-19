fedb05eaa8ba7344b818be385e2d3d7166afb009
Triin Lemba <triin.lemba@advectus.net>
2020-11-19 17:18:24 +0200

    first page and a bit of second page html

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      'sans': ['Helvetica Neue', 'sans-serif']
+    },
+    extend: {
+      backgroundImage: theme => ({
+        'cars-image': "url('/public/img/carsbackground.png')",
+     })
+    },
   },
   variants: {
     extend: {},
